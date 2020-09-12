2a59790790bf5dda89432a75bcc8d5fd4f86bb33
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-09-12 10:33:36 -0400

    fix route and db

--- models/userSessions.js
+++ models/userSessions.js
@@ -2,6 +2,17 @@ module.exports = function (sequelize, DataTypes) {
     const UserSessions = sequelize.define("UserSessions", {
         isPresent: { type: DataTypes.BOOLEAN, defaultValue: 0, allowNull: false},
     });
-
+    UserSessions.associate = function(models) {
+        UserSessions.belongsTo(models.User, {
+          foreignKey: {
+            allowNull: false
+          }
+        });
+        UserSessions.belongsTo(models.CalendarSessions, {
+          foreignKey: {
+          allowNull: false
+          }
+        });
+      };
     return UserSessions;
 };
